397a5010ad49453a19dfa6410755b9a07b79312a
Julio Rafael <juliorafaelnft@hotmail.com>
2025-08-26 23:33:22 -0300

    Enhance FastAPI initialization: add title, description, and versioning to the API in main.py.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,29 @@
 from fastapi import FastAPI
 from src.api import router
 
-app = FastAPI()
+app = FastAPI(
+    title="Sonjis Vision API",
+    description="""
+    WebSocket URL: ws://localhost:8000/v1/stream
+    
+    Response format:
+    
+    {
+        "type": "frame",
+        "data": "base64_encoded_image",
+        "meta": {"id": "1"}
+    }
+    
+    
+    Response format:
+    
+    {
+        "type": "result",
+        "result": {"ok": true, "meta": {"id": "1"}}
+    }
+    
+    """,
+    version="0.0.1"
+)
+
 app.include_router(router)
